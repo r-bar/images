e493df876cab9dc5733fe47fbf8931e870ede360
Ryan Barth <ryan@barth.tech>
2022-06-22 12:49:02 -0700

    simplify build.py

--- build.py
+++ build.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
+import itertools as it
 import json
 import os
 import sys
 from argparse import ArgumentParser
 from pathlib import Path
 from subprocess import run
+from typing import Iterable, TypedDict
 
 PROJECT = "library"
 CONTAINER_REGISTRY = os.environ.get("CONTAINER_REGISTRY", "registry.barth.tech")
@@ -14,18 +16,32 @@ GIT_ROOT = Path(__file__).parent
 IMAGE_DIRECTORY = GIT_ROOT / "images"
 
 
+class Image(TypedDict):
+    name: str
+    fullName: str
+    tag: str
+    repository: str
+    buildArgs: dict[str, str]
+    tagFile: str
+    dockerfile: str
+    imageDir: str
+    context: str
+    registry: str
+    project: str
+
+
 class Cli(ArgumentParser):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.add_argument("command")
-        self.add_argument("image", nargs="?")
-        self.add_argument("tag", nargs="?")
+        self.add_argument("--image")
+        self.add_argument("--tag")
         self.add_argument("--dry-run", action="store_true")
         self.args, self.extra_args = self.parse_known_args()
 
 
-def build_args(image: str, tag: str) -> dict[str, str]:
-    tagfile = IMAGE_DIRECTORY / image / "tags" / tag
+def build_args(tagfile: str | Path) -> dict[str, str]:
+    """Read the given tagfile and return a dictionary of the build arguments"""
     args = {}
     with open(tagfile) as f:
         for line in f:
@@ -34,75 +50,87 @@ def build_args(image: str, tag: str) -> dict[str, str]:
     return args
 
 
-def images():
+def images(
+    image_filter: str | None = None, tag_filter: str | None = None
+) -> Iterable[Image]:
     # print('image directory:', IMAGE_DIRECTORY, file=sys.stderr)
     assert IMAGE_DIRECTORY.is_dir()
     for image_dir in IMAGE_DIRECTORY.iterdir():
         for tag_file in image_dir.joinpath("tags").iterdir():
             if tag_file.name.startswith("."):
                 continue
-            tag_args = build_args(image_dir.name, tag_file.name)
+            if tag_filter and tag_file.name != tag_filter:
+                continue
+            if image_filter and image_dir.name != image_filter:
+                continue
+            repository = f"{CONTAINER_REGISTRY}/{PROJECT}/{image_dir.name}"
             yield {
-                "image": image_dir.name,
+                "fullName": f"{repository}:{tag_file.name}",
+                "name": image_dir.name,
                 "tag": tag_file.name,
-                "repository": f"{CONTAINER_REGISTRY}/{PROJECT}/{image_dir.name}",
-                "buildArgs": " ".join(
-                    f"--build-arg '{k}={v}'" for k, v in tag_args.items()
-                ),
+                "repository": repository,
+                "registry": CONTAINER_REGISTRY,
+                "project": PROJECT,
+                "buildArgs": build_args(tag_file),
+                "tagFile": str(tag_file),
+                "dockerfile": str(image_dir / "Dockerfile"),
+                "imageDir": str(image_dir),
+                "context": str(image_dir / "files"),
             }
 
 
-def build(
-    image: str, tag: str, dry_run: bool = False, extra_args: list[str] | None = None
-):
+def run_build(image: Image, dry_run: bool = False, extra_args: list[str] | None = None):
+    """Run the image build command"""
     extra_args = extra_args or []
-    image_dir = IMAGE_DIRECTORY / image
-    dockerfile = image_dir / "Dockerfile"
-    tagfile = image_dir / "tags" / tag
-    context = image_dir / "files"
     command = [
         CONTAINER_RUNTIME,
         "build",
-        str(context),
+        image["context"],
         "-f",
-        str(dockerfile),
+        image["dockerfile"],
         "-t",
-        f"{CONTAINER_REGISTRY}/{PROJECT}/{image}:{tag}",
+        image["fullName"],
     ]
-    with open(tagfile) as f:
-        for line in f:
-            key, value = line.strip().split("=", maxsplit=1)
-            command += ["--build-arg", f"{key}={value}"]
-    command += extra_args
+    command += list(
+        it.chain.from_iterable(
+            ("--build-arg", f"{k}={v}") for k, v in image["buildArgs"].items()
+        )
+    )
     print(*command)
     if not dry_run:
         run(command, check=True)
 
 
-def build_all(dry_run: bool = False, extra_args: list[str] | None = None):
-    for path in Path("images").glob("*/tags/*"):
-        image = path.parts[1]
-        tag = path.parts[3]
-        build(image, tag, dry_run, extra_args)
+def build(
+    image_filter: str,
+    tag_filter: str,
+    dry_run: bool = False,
+    extra_args: list[str] | None = None,
+):
+    """Build all specified images. Translate cli arguments into """
+    for image in images(image_filter, tag_filter):
+        run_build(image, dry_run, extra_args)
+
+
+def run_push(image: Image, dry_run: bool = False, extra_args: list[str] | None = None):
+    """Run the image push command"""
+    extra_args = extra_args or []
+    command = [CONTAINER_RUNTIME, "push", image["fullName"]]
+    command += extra_args
+    print(*command)
+    if not dry_run:
+        run(command, check=True)
 
 
 def push(
-    image: str, tag: str, dry_run: bool = False, extra_args: list[str] | None = None
+    image_filter: str | None = None,
+    tag_filter: str | None = None,
+    dry_run: bool = False,
+    extra_args: list[str] | None = None,
 ):
-    extra_args = extra_args or []
-    image = f"{CONTAINER_REGISTRY}/{PROJECT}/{image}:{tag}"
-    command = [CONTAINER_RUNTIME, "push", image]
-    command += extra_args
-    print(*command)
-    if not dry_run:
-        run(command, check=True)
-
-
-def push_all(dry_run: bool = False, extra_args: list[str] | None = None):
-    for path in Path("images").glob("*/tags/*"):
-        image = path.parts[1]
-        tag = path.parts[3]
-        push(image, tag, dry_run, extra_args)
+    """Push all specified images to the registry"""
+    for image in images(image_filter, tag_filter):
+        run_push(image, dry_run, extra_args)
 
 
 def main():
@@ -110,14 +138,11 @@ def main():
     match cli.args.command:
         case "build":
             build(cli.args.image, cli.args.tag, cli.args.dry_run, cli.extra_args)
-        case "build-all":
-            build_all(cli.args.dry_run, cli.extra_args)
         case "push":
             push(cli.args.image, cli.args.tag, cli.args.dry_run, cli.extra_args)
-        case "push-all":
-            push_all(cli.args.dry_run, cli.extra_args)
         case "images":
-            json.dump(list(images()), sys.stdout, indent=2)
+            image_data = list(images(cli.args.image, cli.args.tag))
+            json.dump(image_data, sys.stdout, indent=2)
         case _:
             print(f"Unknown command {cli.args.command}")
             cli.print_help()
