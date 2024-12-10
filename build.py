8e6df0389f9e67674b8a0648b73371d555ce3a08
Ryan Barth <ryan@barth.tech>
2024-12-10 00:49:23 -0800

    use tags.toml instead of directory structure

--- build.py
+++ build.py
@@ -4,25 +4,29 @@ import itertools as it
 import json
 import os
 import sys
+import tomllib
+from dataclasses import dataclass, field, asdict
 from argparse import ArgumentParser
 from pathlib import Path
 from subprocess import run
-from typing import Iterable, TypedDict
+from typing import Iterable, Literal
 
 PROJECT = "library"
 CONTAINER_REGISTRY = os.environ.get("CONTAINER_REGISTRY", "registry.barth.tech")
 CONTAINER_RUNTIME = os.environ.get("CONTAINER_RUNTIME", "podman")
 GIT_ROOT = Path(__file__).parent
 IMAGE_DIRECTORY = GIT_ROOT / "images"
+TAG_FILE = "tags.toml"
+VALID_TAG_KEYS = {"args", "dockerfile"}
 
 
-class Image(TypedDict):
+@dataclass(frozen=True, kw_only=True)
+class Image:
     name: str
     fullName: str
     tag: str
     repository: str
-    buildArgs: dict[str, str]
-    tagFile: str
+    buildArgs: dict[str, str] = field(default_factory=dict)
     dockerfile: str
     imageDir: str
     context: str
@@ -30,24 +34,39 @@ class Image(TypedDict):
     project: str
 
 
+@dataclass(frozen=True, kw_only=True)
+class TagData:
+    """Structure for each entry in a tags.toml file"""
+    tag: str
+    dockerfile: str = "Dockerfile"
+    args: dict[str, str] = field(default_factory=dict)
+
+    @classmethod
+    def load(cls, file: Path | str) -> list["TagData"]:
+        tags = []
+        with open(file, "rb") as f:
+            data = tomllib.load(f)
+        for tag, config in data.items():
+            tags.append(cls(tag=tag, **config))
+        return tags
+
+
 class Cli(ArgumentParser):
+    command: Literal["build", "push", "images"]
+    image: str | None
+    tag: str | None
+    dry_run: bool
+    extra_args: list[str]
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.add_argument("command")
         self.add_argument("--image")
         self.add_argument("--tag")
         self.add_argument("--dry-run", action="store_true")
-        self.args, self.extra_args = self.parse_known_args()
-
-
-def build_args(tagfile: str | Path) -> dict[str, str]:
-    """Read the given tagfile and return a dictionary of the build arguments"""
-    args = {}
-    with open(tagfile) as f:
-        for line in f:
-            k, v = line.strip().split("=", maxsplit=1)
-            args[k] = v
-    return args
+        args, self.extra_args = self.parse_known_args()
+        for k, v in vars(args).items():
+            setattr(self, k, v)
 
 
 def images(
@@ -56,28 +75,23 @@ def images(
     # print('image directory:', IMAGE_DIRECTORY, file=sys.stderr)
     assert IMAGE_DIRECTORY.is_dir()
     for image_dir in IMAGE_DIRECTORY.iterdir():
-        for tag_file in image_dir.joinpath("tags").iterdir():
-            if tag_file.name.startswith("."):
-                continue
-            if tag_filter and tag_file.name != tag_filter:
-                continue
-            if image_filter and image_dir.name != image_filter:
-                continue
-            repository = f"{CONTAINER_REGISTRY}/{PROJECT}/{image_dir.name}"
-            rel_image_dir = image_dir.relative_to(GIT_ROOT)
-            yield {
-                "fullName": f"{repository}:{tag_file.name}",
-                "name": image_dir.name,
-                "tag": tag_file.name,
-                "repository": repository,
-                "registry": CONTAINER_REGISTRY,
-                "project": PROJECT,
-                "buildArgs": build_args(tag_file),
-                "tagFile": str(tag_file.relative_to(GIT_ROOT)),
-                "dockerfile": str(rel_image_dir / "Dockerfile"),
-                "imageDir": str(rel_image_dir),
-                "context": str(rel_image_dir / "files"),
-            }
+        rel_image_dir = image_dir.relative_to(GIT_ROOT)
+        repository = f"{CONTAINER_REGISTRY}/{PROJECT}/{image_dir.name}"
+        tag_file = image_dir / TAG_FILE
+        tags = TagData.load(tag_file)
+        for config in tags:
+            yield Image(
+                fullName=f"{repository}:{tag_file.name}",
+                name=image_dir.name,
+                tag=config.tag,
+                repository=repository,
+                registry=CONTAINER_REGISTRY,
+                project=PROJECT,
+                buildArgs=config.args,
+                dockerfile=str(rel_image_dir / config.dockerfile),
+                imageDir=str(rel_image_dir),
+                context=str(rel_image_dir / "files"),
+            )
 
 
 def run_build(image: Image, dry_run: bool = False, extra_args: list[str] | None = None):
@@ -86,11 +100,11 @@ def run_build(image: Image, dry_run: bool = False, extra_args: list[str] | None 
     command = [
         CONTAINER_RUNTIME,
         "build",
-        image["context"],
+        image.context,
         "-f",
-        image["dockerfile"],
+        image.dockerfile,
         "-t",
-        image["fullName"],
+        image.fullName,
     ]
     command += list(
         it.chain.from_iterable(
@@ -136,17 +150,18 @@ def push(
 
 def main():
     cli = Cli()
-    match cli.args.command:
+    match cli.command:
         case "build":
-            build(cli.args.image, cli.args.tag, cli.args.dry_run, cli.extra_args)
+            build(cli.image, cli.tag, cli.dry_run, cli.extra_args)
         case "push":
-            push(cli.args.image, cli.args.tag, cli.args.dry_run, cli.extra_args)
+            push(cli.image, cli.tag, cli.dry_run, cli.extra_args)
         case "images":
-            image_data = list(images(cli.args.image, cli.args.tag))
+            image_data = [asdict(i) for i in images(cli.image, cli.tag)]
             json.dump(image_data, sys.stdout, indent=2)
         case _:
-            print(f"Unknown command {cli.args.command}")
+            print(f"Unknown command {cli.command}")
             cli.print_help()
+            exit(1)
 
 
 if __name__ == "__main__":
